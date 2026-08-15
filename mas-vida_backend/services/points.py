4827711db38abb535904f3252881c162f1cbd5cc
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-08-15 12:46:58 -0600

    points calculations by steps function and validation points functions both were implemented

--- mas-vida_backend/services/points.py
+++ mas-vida_backend/services/points.py
@@ -1,13 +1,31 @@
 
+MAX_DAILY_POINTS = 30
+MIN_STEPS_FOR_5 = 7500
+MIN_STEPS_FOR_10 = 10000
+MIN_STEPS_FOR_20 = 15000
 
-def calculate_points(steps: int):
+
+class InvalidPoints(ValueError):
+    pass
+
+def calculate_points(steps: int)-> int:
+
+    #Calculo de puntos según pasos
     if steps < 0:
-        raise ValueError("Los pasos no pueden ser negativos")
-    elif steps < 7500:
+        raise InvalidPoints()
+    if steps < MIN_STEPS_FOR_5:
         return 0
-    elif steps < 10000:
-        return 50
-    elif steps < 15000:
-        return 100
-    else: return 200
+    if steps < MIN_STEPS_FOR_10:
+        return 5
+    if steps < MIN_STEPS_FOR_20:
+
+        return 10
+    return 20
+
+def apply_daily_points_limit(points: int) -> int:
+    if points > MAX_DAILY_POINTS:
+        return MAX_DAILY_POINTS
+    if points < 0:
+        raise InvalidPoints()
+    return points
 
