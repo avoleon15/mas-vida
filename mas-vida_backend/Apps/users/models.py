1c6290f9d9caa45b2aebc17d7259095551b7f576
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-08-24 22:37:40 -0600

    User Scheme: User, birthdate, policy_number, insurer anbd policy start date were added

--- mas-vida_backend/Apps/users/models.py
+++ mas-vida_backend/Apps/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE
+    )
+
+birthdate = models.DateField()
+
+policy_number = models.CharField(
+    max_length=100
+)
+
+insurer = models.CharField(
+    max_length=100
+)
+
+policy_start_date = models.DateField()
+
+def __str__(self):
+    return self.user.username
+
