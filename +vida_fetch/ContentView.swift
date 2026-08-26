bcfe0a95df5773e8877725d0e87932bac0effe0a
avoleon15 <avoleon15@gmail.com>
2026-08-26 16:03:58 -0600

    Initial Commit

---
+++ +vida_fetch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  +vida_fetch
+//
+//  Created by Alvaro Jose Leon Aguilar on 8/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
