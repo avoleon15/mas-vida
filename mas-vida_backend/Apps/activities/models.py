6a4682b16bb8b1c345544fa5bdfb7f2229fe76fc
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-08-30 19:33:39 -0600

    MuestraBPM table defined

--- mas-vida_backend/Apps/activities/models.py
+++ mas-vida_backend/Apps/activities/models.py
@@ -40,6 +40,43 @@ class Muestra(models.Model):
     def __str__(self):
             return f"{self.usuario} - {self.cantidad} pasos"
 
+
+
+class MuestraBPM(models.Model):
+    usuario = models.ForeignKey(
+            "users.Usuario",
+            on_delete=models.CASCADE
+    )
+
+    external_id = models.CharField(
+            max_length=255
+        )
+
+    inicio = models.DateTimeField()
+    fin = models.DateTimeField()
+
+    bpm = models.IntegerField()
+
+    fuente_bundle = models.CharField(
+          max_length=255
+    )
+
+    fuente_nombre = models.CharField(
+    max_length=255
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["usuario","external_id" ],
+                name="unique_muestra_bpm_external_id"
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.usuario} - {self.bpm} BPM"
+
+
 class Sesion(models.Model):
     usuario = models.ForeignKey(
             "users.Usuario",
