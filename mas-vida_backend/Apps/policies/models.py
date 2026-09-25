4dd727a71adefd210c6862a769adce5cd0c6fe42
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-09-24 22:54:20 -0600

    PolizaVinculada table created

--- mas-vida_backend/Apps/policies/models.py
+++ mas-vida_backend/Apps/policies/models.py
@@ -1,11 +1,38 @@
 from django.db import models
+from Apps.users.models import Usuario
 
 # Create your models here.
-class Policy(models.Model):
+class PolizaVinculada(models.Model):
+    id = models.AutoField(primary_key=True)
 
-    class Status(models.TextChoices):
-        pass
+    usuario = models.OneToOneField(
+        Usuario,
+        on_delete=models.PROTECT,
+        db_column='usuario_id',
+    )
 
-    class PlanType(models.TextChoices):
-        pass
+    policy_number = models.CharField(max_length=100)
+    insurer = models.CharField(max_length=100)
+    policy_start_date = models.DateField()
+    birth_date_confirmado = models.DateField(
+        null=True
+        blank=True
+    )
+    estado_verificación = models.CharField(
+        max_length=10,
+        choices=[
+            ('pendiente', 'pendiente'),
+            ('verificada', 'verificada'),
+            ('rechazada', 'rechazada'),
+        ]
+    )
+
+    fecha_vinculación = models.DateField(auto_now_add=True)
+    fecha_verificación = models.DateField(
+        null= True,
+        blank=True
+    )
+
+
+
 
--- mas-vida_backend/Apps/users/models.py
+++ mas-vida_backend/Apps/users/models.py
@@ -1,29 +1,21 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
-        on_delete=models.PROTECT
+        on_delete=models.PROTECT,
+        db_column="user_id",
     )
-
     usuario_id = models.CharField(
         max_length=100,
-        unique=True
+        unique=True,
     )
-
     birth_date = models.DateField()
-
-    policy_number = models.CharField(
-        max_length=100
-    )
-
-    insurer = models.CharField(
-        max_length=100
-    )
-
-    policy_start_date = models.DateField()
-
+    
     def __str__(self):
         return self.usuario_id
+
